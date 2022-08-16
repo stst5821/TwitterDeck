66517ccd48ef1d934a8bd1400ed5b411e9cf6d48
Shota Toshimitsu <60058290+stst5821@users.noreply.github.com>
2022-08-17 00:31:36 +0900

    indexのデータ取得にeagerロードを追加

--- src/app/Http/Controllers/ArticleController.php
+++ src/app/Http/Controllers/ArticleController.php
@@ -9,11 +9,7 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        // 通常
-        $articles = Article::all()->sortByDesc('created_at');
-
-        // Eagerロード
-        // $articles = Article::with('user')->get()->sortByDesc('created_at');
+        $articles = Article::with('user')->get()->sortByDesc('created_at');
 
         return view('articles.index', compact('articles'));
     }
