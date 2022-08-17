305c2786d5a994c9d1fa7ce23bf8dcf556dd69e6
Shota Toshimitsu <60058290+stst5821@users.noreply.github.com>
2022-08-17 14:26:30 +0900

    create画面作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,18 +7,22 @@ Auth::routes();
 
 // 記事系
 Route::name('articles.')->group(function () {
-  // 一覧、詳細
+  // 一覧
   Route::get('/', 'ArticleController@index')->name('index');
-  Route::get('/articles/{article}', 'ArticleController@show')->name('show');
 
-  // 作成
-  Route::get('/articles/create', 'ArticleController@create')->name('create');
-  Route::post('/articles', 'ArticleController@store')->name('store');
+  Route::middleware(['auth'])->group(function () {
+    // 作成
+    Route::get('/articles/create', 'ArticleController@create')->name('create');
+    Route::post('/articles', 'ArticleController@store')->name('store');
 
-  // 編集
-  Route::get('/articles/{article}/edit', 'ArticleController@edit')->name('edit');
-  Route::post('/articles/{article}', 'ArticleController@update')->name('update');
+    // 詳細
+    Route::get('/articles/{article}', 'ArticleController@show')->name('show');
 
-  // 削除
-  Route::delete('/articles/{article}', 'ArticleController@destroy')->name('destroy');
+    // 編集
+    Route::get('/articles/{article}/edit', 'ArticleController@edit')->name('edit');
+    Route::post('/articles/{article}', 'ArticleController@update')->name('update');
+
+    // 削除
+    Route::delete('/articles/{article}', 'ArticleController@destroy')->name('destroy');
+  });
 });
